5b7d710d3823f571a29cb7664d7dc5d405c553fb
tails142 <derek.r.higgins@gmail.com>
2014-12-30 22:10:34 +0000

    first upload

---
+++ Assets/Standard Assets/Scripts/WaitforClick.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaitforClick : MonoBehaviour {
+
+/*	public MonoBehaviour followScript;
+	public GameObject mainCharacterObject;
+	public GUITexture mainTitle;
+	public GameObject cameraPath;
+*/
+	public GameObject testConnection;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			testConnection.SetActive(true);
+		}
+	
+	}
+}
---
+++ Assets/Standard Assets/Scripts/testConnection.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testConnection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start() {
+		MasterServer.ipAddress = "forums.gullcatlock.com";
+		Network.natFacilitatorIP = MasterServer.ipAddress;
+	}
+
+	void OnFailedToConnectToMasterServer(NetworkConnectionError info)
+	{
+		Debug.Log("Could not connect to master server: " + info);
+
+		WWW www = new WWW("pvlgrvm.gullcatlock.com/checkstatus.php");
+
+
+	}
+
+	void OnServerInitialized()
+	{
+				MasterServer.RegisterHost ("com.pvlgrm", "Dereks Game", "Test");
+	}
+
+	void OnMasterServerEvent(MasterServerEvent msEvent)
+	{
+		Debug.Log(msEvent);
+	}
+
+	void OnGUI () {
+
+		if (GUILayout.Button ("Register"))
+		{
+						bool useNat = !Network.HavePublicAddress ();
+						Network.InitializeServer (32, 25002, useNat);
+		}
+
+		if (GUILayout.Button("Unregister"))
+		{
+			Network.Disconnect();
+			MasterServer.UnregisterHost();
+		}
+	}
+}
